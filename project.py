9998fde7fb5833b458e75b3e9ab3d642935fb85f
Yassin Makrai <makrai.yassin@gmail.com>
2021-09-29 14:51:41 +0200

    slider

--- project.py
+++ project.py
@@ -5,14 +5,13 @@ import matplotlib.pyplot as plt
 import dash
 from dash.dependencies import Input, Output
 from dash import html, dcc
-# from plotly.tools import mpl_to_plotly
+from plotly.tools import mpl_to_plotly
 import plotly.express as px
 matplotlib.use("Agg")
 
 data = pd.read_csv('train.csv')
 
 # print(data[(data['Subject Focus']==1) & (data['Pawpularity'] > 50)] )
-
 # print(data[data["Pawpularity"] < 25])
 # print(data[data["Pawpularity"] > 75])
 # print(data['Pawpularity'].value_counts())
@@ -27,15 +26,34 @@ def image(id):
 def first_graph():
     data['Count']=data['Pawpularity'].value_counts()
     data['Popularity']=data['Pawpularity']
-    fig = px.bar(data, x='Popularity', y='Count')
-    return fig.show()
+    pop=data['Pawpularity'].value_counts()
+    df = pd.concat([pop.index.to_series(), pop], axis=1, keys=['Popularity', 'Count'])
+    fig = px.bar(df, x='Popularity', y='Count')
+    return fig
 
 app.layout = html.Div(
     children=[
         page_title,
-        first_graph()
+        dcc.Graph(figure=first_graph()),
+        dcc.Slider(
+            id="slider", 
+            min=0, 
+            max=100, 
+            step=1, 
+            value=0,
+            marks={ i : f"{i}" for i in range(101)}
+        ),
+        html.P(id="slider_display")
     ]
 )
+
+@app.callback(
+    Output(component_id='slider_display', component_property='children'),
+    [Input(component_id='slider', component_property='value')]
+)
+def display_slider_value(slider_value):
+    message=f"slider value : {slider_value}"
+    return message
 
 if __name__ == "__main__":
     app.run_server(debug = True)
